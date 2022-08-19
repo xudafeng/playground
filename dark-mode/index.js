34ffadfc35c4d0c1ec85b9819975cb466882a2ea
xudafeng <xudafeng@126.com>
2022-08-19 19:59:35 +0800

    feat: add dark mode

---
+++ dark-mode/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+class ThemeManager {
+  constructor() {
+    this.mainKey = 'prefers-color-scheme';
+    this.darkModeMediaQuery = window.matchMedia(`(${this.mainKey}: ${ThemeManager.THEMES.dark})`);
+    this.theme = ThemeManager.THEMES.light;
+    this.init();
+    this.bindEvents();
+  }
+
+  init() {
+    const themefromStorage = window.localStorage.getItem(this.mainKey);
+    if (themefromStorage) {
+      this.theme = themefromStorage;
+    } else {
+      this.theme = this.darkModeMediaQuery.matches ? ThemeManager.THEMES.dark : ThemeManager.THEMES.light;
+    }
+    this.switch(this.theme); 
+  }
+
+  bindEvents() {
+    this.darkModeMediaQuery.addListener((e) => {
+      const isDarkMode = e.matches;
+      this.switch(isDarkMode ? ThemeManager.THEMES.light : ThemeManager.THEMES.dark);
+    });
+  }
+
+  toggle() {
+    const isDarkMode = this.theme === ThemeManager.THEMES.dark;
+    this.switch(isDarkMode ? ThemeManager.THEMES.light : ThemeManager.THEMES.dark);
+  }
+
+  switch(theme = ThemeManager.THEMES.light) {
+    this.theme = theme;
+    window.localStorage.setItem(this.mainKey, this.theme);
+    document.documentElement.setAttribute(this.mainKey, this.theme);
+  }
+}
+
+ThemeManager.THEMES = {
+  light: 'light',
+  dark: 'dark',
+};
+
+const themeManager = new ThemeManager();
+
+document.querySelector('#switch-btn').addEventListener('click', () => {
+  themeManager.toggle();
+}, false);
