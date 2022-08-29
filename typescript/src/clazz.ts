778ca8ec7ff683cbfc3b38b540cfb9be0fb7a468
xudafeng <xudafeng@126.com>
2022-08-29 14:47:09 +0800

    feat: update

--- typescript/src/clazz.ts
+++ typescript/src/clazz.ts
@@ -1,5 +1,11 @@
 import mixin from './mixin';
 
+const Utils = {
+  foo() {
+    console.log('foo');
+  }
+};
+
 class Jumpable {
   z = 0;
   jump() {
@@ -14,13 +20,19 @@ class Duckable {
 class Sprite {
   x = 0;
   y = 0;
+  static Utils = Utils;
 }
- 
+
 interface Sprite extends Jumpable, Duckable {}
 
-mixin(Sprite, [Jumpable, Duckable]);
+mixin(Sprite, [
+  Jumpable,
+  Duckable,
+]);
  
 let player = new Sprite();
 console.log(player.x, player.y, player.z);
 player.jump();
 console.log(player.x, player.y, player.z);
+
+Sprite.Utils.foo();
